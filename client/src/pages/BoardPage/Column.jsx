8bb4cfc7e2cc094e43df2fbbf64d57c22b2522da
Alish-Basnett <np01cp4a210130@islingtoncollege.edu.np>
2024-07-05 17:34:11 +0545

    Enhanced the Board's UI

--- client/src/pages/BoardPage/Column.jsx
+++ client/src/pages/BoardPage/Column.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import "./Column.css";
 import {
   SortableContext,
   verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
-import Task from "./task/Task";
+import { Task } from "./task/Task";
+import "./Column.css";
 
-const Column = ({ tasks }) => {
+const Column = ({ id, tasks }) => {
   return (
     <div className="column">
+      <h2 className="column-title">{id}</h2>
       <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
         {tasks.map((task) => (
-          <Task id={task.id} title={task.title} key={task.id} />
+          <Task key={task.id} id={task.id} title={task.title} />
         ))}
       </SortableContext>
     </div>
--- client/src/pages/BoardPage/task/Task.jsx
+++ client/src/pages/BoardPage/task/Task.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import "./Task.css";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 
-const Task = ({ id, title }) => {
+import "./Task.css";
+
+export const Task = ({ id, title }) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id });
 
@@ -15,14 +15,13 @@ const Task = ({ id, title }) => {
   return (
     <div
       ref={setNodeRef}
+      style={style}
       {...attributes}
       {...listeners}
-      style={style}
       className="task"
     >
+      <input type="checkbox" className="checkbox" />
       {title}
     </div>
   );
 };
-
-export default Task;
